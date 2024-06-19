392eed0448a01f5a6a1e6ba4024df3cde93ee0a4
AmΔn <134101408+Amanbasnet08@users.noreply.github.com>
2024-06-19 16:55:13 -0400

    Delete TEST2B directory

--- TEST2B/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.test2b {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens com.example.test2b to javafx.fxml;
-    exports com.example.test2b;
-}
